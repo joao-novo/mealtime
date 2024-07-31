f8af6a4a633c435364ce0f7119124843dfb70f8a
João Novo <joaonovo05@protonmail.com>
2024-07-31 10:47:38 +0100

    fix: removed warning

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use crate::api::database::handle_database_connection;
 use std::io;
 
-fn handle_commands(command: String) {}
+pub mod api;
+fn handle_commands(command: String) {
+    todo!();
+}
 
 fn main() {
-    // handle_database_connection();
+    handle_database_connection();
     loop {
         let mut command = String::new();
         match io::stdin().read_line(&mut command) {
