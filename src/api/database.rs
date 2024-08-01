9e49370a5a3db9d8ba821091fc3b57c3b42689c3
João Novo <joaonovo05@protonmail.com>
2024-08-01 11:44:20 +0100

    fix: put connection into a struct to make borrowing easier

--- src/api/database.rs
+++ src/api/database.rs
@@ -1,10 +1,14 @@
 use rusqlite::Connection;
 
-pub fn handle_database_connection() -> Connection {
-    match Connection::open("db/menu.db") {
+pub struct Database {
+    pub connection: Connection,
+}
+
+pub fn handle_database_connection() -> Database {
+    match Connection::open("./db/menu.db") {
         Ok(conn) => {
             println!("Connection established");
-            return conn;
+            return Database { connection: conn };
         }
         Err(_) => panic!("Could not connect to database"),
     }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use crate::api::database::handle_database_connection;
+use crate::api::database::{handle_database_connection, Database};
 use std::io;
 
 pub mod api;
-fn handle_commands(command: String) {
+fn handle_commands(db: &Database, command: String) {
     todo!();
 }
 
 fn main() {
-    handle_database_connection();
+    let db = handle_database_connection();
     loop {
         let mut command = String::new();
         match io::stdin().read_line(&mut command) {
             Ok(_) => {
-                handle_commands(command);
+                handle_commands(&db, command);
             }
             Err(error) => {
                 panic!("Error reading input: {error:?}")
